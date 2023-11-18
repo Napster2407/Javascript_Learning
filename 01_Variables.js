f3d856f734b5766713dcd39d3c6428529ce46796
Shrey Pandey <shreypandey@Shreys-MacBook-Air.local>
2023-11-18 23:31:50 +0530

    Variables

---
+++ 01_Variables.js
@@ -0,0 +1,8 @@
+const UserId=1000012340;
+let UserName="Shrey";
+var UserAddress="Kanpur";
+State="UP";
+let Password
+
+console.table([UserId,UserName,UserAddress,State,Password]);
+//Do not use Var as it's a old standard
---
+++ Test.js
@@ -0,0 +1,2 @@
+console.log("Shrey");
+console.log("Pandey")
