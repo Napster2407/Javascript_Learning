c3866f9a8dce391887e6643b927dfb9249839e56
Shrey Pandey <shreypandey@Shreys-MacBook-Air.local>
2023-11-20 00:07:31 +0530

    DataType

---
+++ 02_DataType.js
@@ -0,0 +1,18 @@
+let name ="Shrey"
+let num=4332
+let real=true
+
+console.table([name, num, real])
+/*Primitive Data Type
+number
+bigint
+String
+boolean
+null
+undefined
+Symbols=> genereally used in React
+
+*/
+//Objects
+
+console.log(typeof null) // here it will Give object
